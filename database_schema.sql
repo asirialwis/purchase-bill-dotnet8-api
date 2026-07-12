a91fa21f59db2a2b781628106d8ed17268022e4c
asirialwis <asirichathurika900@gmail.com>
2026-07-12 18:43:56 +0530

    feat: add initial database schema for LocationDetails and migration history

---
+++ database_schema.sql
@@ -0,0 +1,27 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [LocationDetails] (
+    [Location_Code] nvarchar(450) NOT NULL,
+    [Location_Name] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_LocationDetails] PRIMARY KEY ([Location_Code])
+);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20260710163621_InitialLocationTable', N'8.0.28');
+GO
+
+COMMIT;
+GO
+
